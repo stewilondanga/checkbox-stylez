f01f1cc2bfe703806bd7808e992ecf48046b931d
Stewart ilondanga <stilondanga@gmail.com>
2018-08-26 20:38:22 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
 
 // All this jquery is just used for presentation. Not required at all for the radio buttons to work.
-$( document ).ready(function(){
-//   Hide the border by commenting out the variable below
-    var $on = 'section';
-    $($on).css({
-      'background':'none',
-      'border':'none',
-      'box-shadow':'none'
-    });
+$(document).ready(function() {
+  //   Hide the border by commenting out the variable below
+  var $on = 'section';
+  $($on).css({
+    'background': 'none',
+    'border': 'none',
+    'box-shadow': 'none'
+  });
 });
