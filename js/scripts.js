4c4f06f11a3b134c2f572e8bd773846b4be33846
Stewart ilondanga <stilondanga@gmail.com>
2018-08-26 19:42:36 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,14 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+// All this jquery is just used for presentation. Not required at all for the radio buttons to work.
+$( document ).ready(function(){
+//   Hide the border by commenting out the variable below
+    var $on = 'section';
+    $($on).css({
+      'background':'none',
+      'border':'none',
+      'box-shadow':'none'
+    });
+});
